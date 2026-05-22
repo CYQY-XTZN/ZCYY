76ea729cab05a1e38ee2734d5cb8feff14a2130f
Gege Kang <gzkang@ucdavis.edu>
2026-05-21 21:32:58 -0700

    2nd

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,11 +17,11 @@ export default function HeroSection() {
         <div>
           <h1 className="text-4xl md:text-5xl lg:text-5xl font-bold text-slate-900 leading-[1.1] mb-9">
             道地瑶药——<br />
-            <span className="whitespace-nowrap opacity-90">瑶族是具有悠久历史和灿烂文化的民族</span>
+            <span className="whitespace-nowrap text-black opacity-90">瑶族是具有悠久历史和灿烂文化的民族</span>
           </h1>
           <p className="text-gray-500 text-2xl mb-10 max-w-none leading-relaxed">
             <span className="block whitespace-nowrap">在长期同疾病斗争的过程中形成和发展起来的民族传统医药——瑶医药，</span>
-            <span className="block">是瑶族灿烂文化的组成部分，也是我们传统医药的重要组成部分。</span>
+            <span className="block whitespace-nowrap">是瑶族灿烂文化的组成部分，也是我们传统医药的重要组成部分。</span>
           </p>
           <button className="flex items-center gap-2 bg-[#333] text-white px-8 py-3 rounded-full text-sm hover:bg-black transition shadow-xl">
             <Link href="#contact" >
